7638c63c2252bf890825728f4ed3d07198c95b84
P€pperMCGamer$ Ch. PmgTeam <113534696+pmgdev64@users.noreply.github.com>
2026-04-09 19:22:11 +0700

    embedded app

--- src/applications/echo.c
+++ src/applications/echo.c
@@ -1,1 +1,10 @@
+#include "../vga_buffer.h"
 
+void app_echo(char *args) {
+    if (args[0] == '\0') {
+        vga_puts("\n", 0);
+        return;
+    }
+    vga_puts(args, 0x07); // In tham số với màu xám trắng
+    vga_puts("\n", 0);
+}
